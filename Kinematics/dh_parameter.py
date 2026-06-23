288e4929672fe8a616f8e042cc8f17a6f4f09ffc
Anooj Jilladwar <anoojjilladwar.com>
2026-06-23 09:55:51 +0530

    dh_parameter added-1

--- Kinematics/dh_parameter.py
+++ Kinematics/dh_parameter.py
@@ -1,28 +1,22 @@
 import numpy as np 
-from numpy import * 
 
-
-def dh_matrix(theta, d, a, alpha):
-
+def dh_parameter(theta, d, r, alpha):
     ct = np.cos(theta)
     st = np.sin(theta)
-
+    
     ca = np.cos(alpha)
     sa = np.sin(alpha)
-
-    T = np.array([
-        [ct, -st*ca, st*sa, a*ct],
-        [st, ct*ca, -ct*sa, a*st],
-        [0, sa, ca, d],
-        [0, 0, 0, 1]
-    ])
-
-    return T
-
+    
+    T = np.array([[ct, -st*ca, st*sa, r*ct],
+                  [st, ct*ca, -ct*sa, r*st],
+                  [0, sa, ca, d],
+                  [0, 0, 0, 1]])
+    
+    return T 
 
 theta = np.radians(45)
 alpha = np.radians(90)
 
-T = dh_matrix(theta, 10, 5, alpha)
-
-print(T)
+X = dh_parameter(theta, 10, 5, alpha)
+print(X)
+    
